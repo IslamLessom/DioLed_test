580647cee3a5ad3830831d5bc00efbfb7c9c155a
ruslanmakiev <ruslanmakiev@ruslanmakievs-MacBook-Air.local>
2024-11-21 10:52:11 +0300

    Bag fix in mibile version in Sidebar

--- client/src/widgets/sidebar/ui/Sidebar.tsx
+++ client/src/widgets/sidebar/ui/Sidebar.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import MenuComponent from "./comonents/menu/Menu";
 import Liders from "./comonents/liders/Liders";
+import { useMediaQuery } from "@/shared/hooks/useMediaQuery";
 import styles from "./Sidebar.module.scss";
 
 const Sidebar = () => {
+  const isMobile = useMediaQuery("(max-width: 768px)");
   return (
     <div className={styles.sidebar}>
       <MenuComponent />
-      <Liders />
+
+      {!isMobile && <Liders />}
     </div>
   );
 };
