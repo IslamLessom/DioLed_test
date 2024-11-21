4c1914e3c71abf82c39c14c508635768694cac1f
ruslanmakiev <ruslanmakiev@ruslanmakievs-MacBook-Air.local>
2024-11-21 11:05:19 +0300

    Card fix size

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -3,9 +3,7 @@ import { FurnitureGrid } from "@/widgets/furniture-categories";
 export default function Home() {
   return (
     <>
-      <h1>
-        <FurnitureGrid />
-      </h1>
+      <FurnitureGrid />
     </>
   );
 }
