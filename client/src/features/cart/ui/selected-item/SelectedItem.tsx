f72ac5b556e2752abd0391255d1b8f1d7ba7f6b4
ruslanmakiev <ruslanmakiev@ruslanmakievs-MacBook-Air.local>
2024-12-02 10:43:44 +0300

    edit busket page adaptive version

--- client/src/features/cart/ui/selected-item/SelectedItem.tsx
+++ client/src/features/cart/ui/selected-item/SelectedItem.tsx
@@ -7,7 +7,7 @@ import { CartPage } from "../cart-page/CartPage";
 import { useMediaQuery } from "@/shared/hooks/useMediaQuery";
 
 export const SelectedItem = () => {
-  const isMobile = useMediaQuery("(max-width: 768px)");
+  const isMobile = useMediaQuery("(max-width: 1024px)");
 
   const onChange: CheckboxProps["onChange"] = (e) => {
     console.log(`checked = ${e.target.checked}`);
@@ -36,8 +36,10 @@ export const SelectedItem = () => {
                 <p className={styles.sekected__card_item_name}>
                   Наименование товара и описание
                 </p>
-                <p>Цена</p>
-                <p>Сумма</p>
+                <div className={styles.sekected__card_item_name_container}>
+                  <p>Цена</p>
+                  <p>Сумма</p>
+                </div>
               </div>
             ) : (
               <Checkbox onChange={onChange}>Выбрать все</Checkbox>
