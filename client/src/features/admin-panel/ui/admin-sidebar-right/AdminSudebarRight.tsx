4b80ead49abd133d42bdfdb1c58b9df7929bc36d
ruslanmakiev <ruslanmakiev@ruslanmakievs-MacBook-Air.local>
2024-12-08 12:18:17 +0300

    admin-sidebar-right

--- client/src/features/admin-panel/ui/admin-sidebar-right/AdminSudebarRight.tsx
+++ client/src/features/admin-panel/ui/admin-sidebar-right/AdminSudebarRight.tsx
@@ -1,7 +1,25 @@
 import React from "react";
-
+import { FaFileCsv } from "react-icons/fa";
+import styles from "./AdminSudebarRight.module.scss";
+import { MdDeleteSweep, MdFormatListBulletedAdd } from "react-icons/md";
 const AdminSudebarRight = () => {
-  return <div>AdminSudebarRight</div>;
+  return (
+    <div className={styles.sudebar_right}>
+      <p>Экспорт</p>
+      <div className={styles.csv}>
+        <FaFileCsv />
+        CSV
+      </div>
+      <div className={styles.csv}>
+        <MdDeleteSweep />
+        Удалить товары
+      </div>
+      <div className={styles.csv}>
+        <MdFormatListBulletedAdd />
+        Добавить товары
+      </div>
+    </div>
+  );
 };
 
 export default AdminSudebarRight;
