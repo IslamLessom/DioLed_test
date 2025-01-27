1f5b635c6a93becac8e7e76ccfa2137e7986b551
ruslanmakiev <ruslanmakiev@ruslanmakievs-MacBook-Air.local>
2025-01-27 17:31:42 +0300

    auth page edit

--- client/src/app/auth/page.tsx
+++ client/src/app/auth/page.tsx
@@ -5,6 +5,7 @@ import { Button, Card, Input } from "antd";
 import Link from "next/link";
 
 import styles from "./page.module.scss";
+import Image from "next/image";
 
 const Auth = () => {
   const [isLogin, setIsLogin] = useState(true);
@@ -13,51 +14,62 @@ const Auth = () => {
     setIsLogin(!isLogin);
   };
   return (
-    <Card className={styles.auth}>
-      <h1 className={styles.auth__title}>{isLogin ? "Вход" : "Регистрация"}</h1>
-      <div className={styles.auth__container}>
-        <Input className={styles.auth__input} placeholder="Введите почту" />
-        {!isLogin && (
-          <Input className={styles.auth__input} placeholder="Введите имя" />
-        )}
-        <Input.Password
-          className={styles.auth__input}
-          placeholder="Введите пароль"
-        />
-        {!isLogin && (
+    <div className={styles.authContainer}>
+      <Card className={styles.auth}>
+        <h1 className={styles.auth__title}>Личный кабинет </h1>
+        <div className={styles.auth__container}>
+          <Input className={styles.auth__input} placeholder="Введите почту" />
+          {!isLogin && (
+            <Input className={styles.auth__input} placeholder="Введите имя" />
+          )}
           <Input.Password
-            className={styles.auth__input_password}
-            placeholder="Повторите пароль"
+            className={styles.auth__input}
+            placeholder="Введите пароль"
           />
-        )}
+          {!isLogin && (
+            <Input.Password
+              className={styles.auth__input_password}
+              placeholder="Повторите пароль"
+            />
+          )}
+        </div>
+        <div className={styles.auth__button_container}>
+          <Button
+            className={styles.auth__button}
+            type="primary"
+            style={{ marginTop: "10px" }}
+          >
+            {isLogin ? "Войти" : "Зарегистрироваться"}
+          </Button>
+        </div>
+        <p className={styles.auth__description} style={{ marginTop: "10px" }}>
+          {isLogin ? (
+            <>
+              Нет аккаунта?{" "}
+              <Button type="link" onClick={toggleMode}>
+                Зарегистрируйтесь
+              </Button>
+            </>
+          ) : (
+            <>
+              Уже есть аккаунт?{" "}
+              <Button type="link" onClick={toggleMode}>
+                <Link href="/profile">Войдите</Link>
+              </Button>
+            </>
+          )}
+        </p>
+      </Card>
+      <div className={styles.div}>
+        <Image
+          className={styles.imagebody}
+          src={"/auth.png"}
+          alt=""
+          width={100}
+          height={100}
+        />
       </div>
-      <div className={styles.auth__button_container}>
-        <Button
-          className={styles.auth__button}
-          type="primary"
-          style={{ marginTop: "10px" }}
-        >
-          {isLogin ? "Войти" : "Зарегистрироваться"}
-        </Button>
-      </div>
-      <p className={styles.auth__description} style={{ marginTop: "10px" }}>
-        {isLogin ? (
-          <>
-            Нет аккаунта?{" "}
-            <Button type="link" onClick={toggleMode}>
-              Зарегистрируйтесь
-            </Button>
-          </>
-        ) : (
-          <>
-            Уже есть аккаунт?{" "}
-            <Button type="link" onClick={toggleMode}>
-              <Link href="/profile">Войдите</Link>
-            </Button>
-          </>
-        )}
-      </p>
-    </Card>
+    </div>
   );
 };
 
--- client/src/app/profile/page.tsx
+++ client/src/app/profile/page.tsx
@@ -10,12 +10,11 @@ import { AdminPage } from "@/features/AdminPanel/ui/admin-page/AdminPage";
 const Profile = () => {
   return (
     <div className={styles.profileContainer}>
-      {/*<p>1 - вход </p>
       <Auth />
-      <p>2 - обычный пользователь видит эту страницу</p>
-      */}
+      {/* 
       <UserProfileInfo />
       <OrderStatus />
+       */}
       {/*
       <p>3 - админ видит эту страницу</p>
       <AdminPage />
