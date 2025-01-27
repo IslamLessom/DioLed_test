8e1a35e7a973fe91f8976060fbdd9c1a61e90db9
ruslanmakiev <ruslanmakiev@ruslanmakievs-MacBook-Air.local>
2025-01-27 14:26:39 +0300

    edit mobile version profile page

--- client/src/features/UserProfile/components/user-profile/UserProfileInfo.tsx
+++ client/src/features/UserProfile/components/user-profile/UserProfileInfo.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import styles from "./UserProfileInfo.module.scss";
+import { Carousel } from "antd";
 const UserProfileInfo = () => {
   return (
     <div className={styles.profile}>
@@ -16,24 +17,47 @@ const UserProfileInfo = () => {
       <div className={styles.profile__delivery_list}>
         <div className={styles.profile__delivery_list__title}>мои заказы</div>
         <div className={styles.profile__delivery_list__container}>
-          <div className={styles.profile__delivery_list__container__product}>
-            <Image src="/bra.jpg" alt="" width={100} height={100} />
-            <p>Angelpoise</p>
-            <p>Бра</p>
-            <h3>12.800$</h3>
-          </div>
-          <div className={styles.profile__delivery_list__container__product}>
-            <Image src="/bra.jpg" alt="" width={100} height={100} />
-            <p>Angelpoise</p>
-            <p>Бра</p>
-            <h3>12.800$</h3>
-          </div>
-          <div className={styles.profile__delivery_list__container__product}>
-            <Image src="/bra.jpg" alt="" width={100} height={100} />
-            <p>Angelpoise</p>
-            <p>Бра</p>
-            <h3>12.800$</h3>
-          </div>
+          <Carousel
+            infinite
+            draggable
+            swipeToSlide
+            slidesToShow={3}
+            autoplay={false}
+            responsive={[
+              {
+                breakpoint: 1250,
+                settings: {
+                  slidesToShow: 1,
+                },
+              },
+              {
+                breakpoint: 768,
+                settings: {
+                  slidesToShow: 1,
+                },
+              },
+            ]}
+            className={styles.profile__delivery_list__container__carousel}
+          >
+            <div className={styles.profile__delivery_list__container__product}>
+              <Image src="/bra.jpg" alt="" width={100} height={100} />
+              <p>Angelpoise</p>
+              <p>Бра</p>
+              <h3>12.800$</h3>
+            </div>
+            <div className={styles.profile__delivery_list__container__product}>
+              <Image src="/bra.jpg" alt="" width={100} height={100} />
+              <p>Angelpoise</p>
+              <p>Бра</p>
+              <h3>12.800$</h3>
+            </div>
+            <div className={styles.profile__delivery_list__container__product}>
+              <Image src="/bra.jpg" alt="" width={100} height={100} />
+              <p>Angelpoise</p>
+              <p>Бра</p>
+              <h3>12.800$</h3>
+            </div>
+          </Carousel>
         </div>
       </div>
     </div>
